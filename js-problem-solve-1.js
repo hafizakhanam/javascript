e92cda7bdfc17b43e1a288dca7d1b1c19a323de3
hafizakhanam <hafiza.khanam0@gmail.com>
2023-04-01 18:17:17 +0600

    first commit

--- js-problem-solve-1.js
+++ js-problem-solve-1.js
@@ -393,3 +393,14 @@ function mergeLeft(ar1, ar2) {
 }
 
 // knjhoigfhcfn
+
+const originalArray = [1, 6, 2, 7, 3, 8, 4, 9, 5, 10];
+
+const newArray = originalArray.map(value => {
+  if (value < 5) {
+    return value + 10;
+  }
+  return value;
+});
+
+console.log(newArray);
